e89e6d43558d1ce174a262cb1621dd6fbf3f601c
jailson Mendonça <jailsonmendonca0707@gmail.com>
2023-05-04 18:38:14 -0100

    Api salvar email com autorização

--- pages/api/api_email.js
+++ pages/api/api_email.js
@@ -1,17 +1,38 @@
 import { PrismaClient } from '@prisma/client'
+import cors from 'cors'
 
-export default async function saveData(req, res) {
-  if (req.method === 'POST') {
-    const prisma = new PrismaClient()
-    const {email} = req.body
-    console.log(req.body)
-    const user = await prisma.user.create({
-      data: {
-        email
-      },
-    })
-    res.json({ user })
- } else{
-  res.status(405).send('Mensagem: Metodo privado')
- }
+
+const corsMiddleware = cors({
+  origin: '*'
+})
+
+
+export async function saveData(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  
+  
+  const token = req.headers.authorization
+  //console.log();
+ if( token=== `${process.env.TOKEN}`) {
+      const prisma = new PrismaClient()
+      const {email} = req.body
+      //console.log(token)
+      const user = await prisma.user.create({
+        data: {
+          email
+        },
+      })
+      res.json({ user })
+  } else{
+    res.status(200).send('Mensagem: Metodo privado')
+  }
 }
+
+// Adicionar o middleware cors() antes de definir a rota saveData()
+export default function(req, res) {
+  corsMiddleware(req, res, () => {
+
+    saveData(req, res)
+  })
+}
+
